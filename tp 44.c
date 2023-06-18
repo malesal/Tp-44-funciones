d906aed72faa995d6b439804f42864f80461e4ff
malesal <133693731+malesal@users.noreply.github.com>
2023-06-18 19:09:52 -0300

    Update tp 44.c

--- tp 44.c
+++ tp 44.c
@@ -1,22 +1,22 @@
 #include<iostream>
 #include<string>
 using namespace std;
-
+float CalcularPromedio(float nota1, float nota2, float nota3);
 int main()
 {
 string nombre;
-float notal, nota2, nota3, promedio;
+float nota1, nota2, nota3, promedio;
 
 cout << "Ingrese el nombre del alumno: ";
 getline (cin, nombre);
 cout << "Ingrese la primera nota: " ;
-cin >> notal;
+cin >> nota1;
 cout << "Ingrese la segunda nota: ";
 cin >> nota2;
 cout << "Ingrese la tercera nota: ";
 cin >> nota3;
 
-promedio = (notal + nota2 + nota3) / 3;
+promedio=CalcularPromedio(nota1, nota2, nota3);
 if (promedio < 6)
 {
 cout << "El alumno" << nombre << " desaprobo con un promedio de " << promedio << endl;
@@ -27,5 +27,9 @@ cout << "El alumno" << nombre << " aprobo con un promedio de " << promedio << en
   cout << "El alumno " << nombre << " obtuvo un excelente con un promedio de " << promedio << endl;
 }
 }
+float CalcularPromedio(float nota1, float nota2, float nota3){
+float promedio = (nota1 + nota2 + nota3) / 3;
+return promedio;
 
+}
 
